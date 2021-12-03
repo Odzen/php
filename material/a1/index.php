5f95b0a10489811d33b59cfdab21832f014756fe
odzen <jsebastian.va@gmail.com>
2021-12-03 02:09:20 -0500

    Add some files

---
+++ material/a1/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercise 1</title>
+</head>
+<body>
+<p> Hello World!</p>
+
+        <?php
+            echo 
+            <p>
+                Nombre: Juan Sebastian Velasquez Acevedo
+                <br/>
+                CC: 1005869667
+                <br/>
+                Hola querido profesor Juan Jose Murillo Aguirre, un placer conocerlo, que pena por la demora
+            </p>
+        ?>
+
+    </body>
+
+    
+</body>
+</html>
