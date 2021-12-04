f82326da7b62dbd61a7ed7f89ab636ed35d0af1f
odzen <jsebastian.va@gmail.com>
2021-12-04 02:39:25 -0500

    Creación de un array multidimensional

--- ejercicios/ejercicio4.php
+++ ejercicios/ejercicio4.php
@@ -1,34 +1,46 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Unidad 2 - Ejemplo 8 - 1</title>
+        <title>Ejercicio 4</title>
         <meta http-equiv="Content-Type" 
               content="text/html; charset=ISO-8859-1" />
     </head>
     <body>
-        <h3>Ejemplo de arreglo con llaves num&eacute;ricas</h3>
+        <h3>Ejemrcicio 4 - Arreglos multidimensionales</h3>
         <?php
-        */*cree un arreglo que contenga un pequeño directorio
+        /*cree un arreglo que contenga un pequeño directorio
 telefónico con 3 personas con los siguientes datos: Nombre, Dirección, Teléfono y Fecha
 de cumpleaños. Muestre todos los datos del arreglo en el navegador.
 Nota: para este ejercicio es necesario buscar información sobre cómo se crea un array
-multidimensional.
-         */
+multidimensional. */
+         
         
         $listadoProductosCodigoNombre = array(
-            array()
-            "Nombre" => "Arroz x libra",
-            "Direccion" => "Frasco aceite x 1000 mls.",
-            "Telefono"=> "Mantequilla x 250 grs.",
-	    "Fecha de Cumpleanos" => ""
+            array("Nombre" => "Juanse",
+                "Direccion" => "123",
+                "Telefono"=> 1234,
+	            "Fecha de Cumpleanos" => "29/08/2000"),
+            array("Nombre" => "David",
+                "Direccion" => "123",
+                "Telefono"=> 1234,
+	            "Fecha de Cumpleanos" => "29/08/2000"),
+            array("Nombre" => "Marcos",
+                "Direccion" => "123",
+                "Telefono"=> 1234,
+	            "Fecha de Cumpleanos" => "29/08/2000")
+            
         );
+
+        print_r($listadoProductosCodigoNombre);
         ?>
+        
         <p>
             <?php
-            $codigoProducto = 1234;
-            echo "El producto con c&oacute;digo $codigoProducto es: "
-            . "$listadoProductosCodigoNombre[$codigoProducto]";
+            $name= 'Nombre';
+            $firstName = $listadoProductosCodigoNombre[1][$name];
+            echo "El primer nombre es: $firstName" ;
             ?>
         </p> 
+        
     </body>
 </html>
